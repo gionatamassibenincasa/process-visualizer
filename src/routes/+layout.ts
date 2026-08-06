f6df7dde5c2a9783e75b007925d51d44d3c88a76
Gionata Massi <gionata@users.noreply.github.com>
2026-08-06 10:50:43 +0200

    Add GitHub pages support

---
+++ src/routes/+layout.ts
@@ -0,0 +1,4 @@
+// import type { LayoutLoad } from './$types';
+
+export const prerender = true;
+export const ssr = true;
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -15,7 +15,17 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter(),
+			adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html', // Consente il routing Client-Side / SPA fallback
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			// Gestisce il sottopackage /nome-repo/ su GitHub Pages
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
+		},
 			experimental: { remoteFunctions: true }
 		})
 	]
